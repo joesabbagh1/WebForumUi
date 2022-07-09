31f878298467b008cc6bd6267ebe8c7bf4d9746a
jpkhawam <jeanpaulkhawam@tutanota.com>
2022-07-09 17:25:19 +0300

    preparing for shared services

---
+++ src/app/shared/comments/comment.ts
@@ -0,0 +1,7 @@
+export class Comment {
+  id!: string
+  user_id!: string
+  post_id!: string
+  content!: string
+  date_created!: string
+}
---
+++ src/app/shared/replies/reply.ts
@@ -0,0 +1,7 @@
+export class Reply {
+  id!: string
+  user_id!: string
+  comment_id!: string
+  content!: string
+  date_created!: string
+}
