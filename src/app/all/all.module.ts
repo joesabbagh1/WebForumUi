27c3d87dd4fde42cb8ab6ae8232061e58bfe7281
jpkhawam <jeanpaulkhawam@tutanota.com>
2022-07-19 14:57:55 +0300

    loading animation, "no posts" message

--- src/app/all/all.module.ts
+++ src/app/all/all.module.ts
@@ -6,19 +6,21 @@ import {AllComponent} from './all.component';
 import {MatDividerModule} from "@angular/material/divider";
 import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
+import {MatProgressBarModule} from "@angular/material/progress-bar";
 
 
 @NgModule({
   declarations: [
     AllComponent
   ],
-  imports: [
-    CommonModule,
-    AllRoutingModule,
-    MatDividerModule,
-    MatCardModule,
-    MatButtonModule
-  ]
+    imports: [
+        CommonModule,
+        AllRoutingModule,
+        MatDividerModule,
+        MatCardModule,
+        MatButtonModule,
+        MatProgressBarModule
+    ]
 })
 export class AllModule {
 }
